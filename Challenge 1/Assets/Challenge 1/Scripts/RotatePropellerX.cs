1a03560bdae6e4be02e40d3498162261119dd69e
Rakner <racnarokg@gmail.com>
2022-06-02 21:48:24 +0100

    Uploading

---
+++ Challenge 1/Assets/Challenge 1/Scripts/RotatePropellerX.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatePropellerX : MonoBehaviour
+{
+    public float rotationSpeed = 5.0f;
+    float a = 0.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward * rotationSpeed);
+    }
+}
